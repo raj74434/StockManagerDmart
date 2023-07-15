333beb6793a0603b154af6c25175875d50a90a2b
Raj Gupta <105916680+raj74434@users.noreply.github.com>
2023-07-14 21:24:15 -0700

    project created with package & dependency

---
+++ Dmart/src/main/java/com/storeManager/Dmart/DmartApplication.java
@@ -0,0 +1,13 @@
+package com.storeManager.Dmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmartApplication.class, args);
+	}
+
+}
